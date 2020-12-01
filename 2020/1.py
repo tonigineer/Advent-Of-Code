d03918ccfc9399260ef1c6d0be6e500e7f756faf
ingenieurtoni <toni@lubiniecki.de>
2020-12-01 16:27:09 +0100

    Added: final approach for day 1

--- 2020/1.py
+++ 2020/1.py
@@ -1,25 +1,12 @@
-"""https://adventofcode.com/2020/day/1."""
+""" https://adventofcode.com/2020/day/1 """
+
 import aocd
 import itertools
 import numpy as np
 
 
-def part1(numbers, targetValue=2020):
-    for number in numbers:
-        for number2 in numbers:
-            if number + number2 == targetValue:
-                return number * number2
-
-
-def part2(numbers, targetValue=2020):
-    for number in numbers:
-        for number2 in numbers:
-            for number3 in numbers:
-                if number + number2 + number3 == targetValue:
-                    return number * number2 * number3
-
-
-def refactored(numbers, depth, targetValue=2020):
+def solvePuzzle(numbers, depth, targetValue=2020):
+    """Generic approach to solve puzzle."""
     for values in itertools.combinations(numbers, depth):
         if sum(values) == targetValue:
             return np.prod(values)
@@ -28,14 +15,5 @@ def refactored(numbers, depth, targetValue=2020):
 if __name__ == "__main__":
     data = aocd.get_data(day=1, year=2020)
     numbers = list(map(int, data.splitlines()))
-    print('Simple approach')
-    print(f'Solution 1: {part1(numbers)}')
-    print(f'Solution 2: {part2(numbers)}')
-
-    print('\nOneliner')
-    print(f'Solution 1: {[a*b for (a, b) in itertools.combinations(numbers, 2) if a+b == 2020][0]}')
-    print(f'Solution 2: {[a*b*c for (a, b, c) in itertools.combinations(numbers, 3) if a+b+c == 2020][0]}')
-
-    print('\nGeneric refactored')
-    print(f'Solution 1: {refactored(numbers, 2)}')
-    print(f'Solution 2: {refactored(numbers, 3)}')
+    print(f'Part 1: {solvePuzzle(numbers, 2)}')
+    print(f'Part 2: {solvePuzzle(numbers, 3)}')
