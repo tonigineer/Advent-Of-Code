2128b07a0c705d1335b8e3430e5c2242e01b7057
Toni Lubiniecki <toni@lubiniecki.de>
2021-12-01 18:46:10 +0100

    little refactoring

--- 2021/1.cpp
+++ 2021/1.cpp
@@ -6,12 +6,13 @@
 #define INPUT_FILENAME "data/1.in"
 
 
+std::vector<std::string> lines = read_lines_from_file(INPUT_FILENAME);
+std::vector<int> depths = convert_strings_to_integer(lines);
+
+
 int part1()
 {
-    std::vector<std::string> lines = read_lines_from_file(INPUT_FILENAME);
-    std::vector<int> depths = convert_strings_to_integer(lines);
     int counter = 0;
-
     for (auto depth = depths.begin() + 1; depth != depths.end(); ++depth)
     {
         if (*depth > *(depth - 1)) ++counter;
@@ -23,16 +24,13 @@ int part1()
 
 int part2()
 {
-    std::vector<std::string> lines = read_lines_from_file(INPUT_FILENAME);
-    std::vector<int> depths = convert_strings_to_integer(lines);
-    int counter = 0;
-
     std::vector<int> sliding_windows;
     for (auto depth = depths.begin() + 2; depth != depths.end(); ++depth)
     {
         sliding_windows.push_back(*depth + *(depth-1) + *(depth-2));
     }
 
+    int counter = 0;
     for (auto window = sliding_windows.begin() + 1; window != sliding_windows.end(); ++window)
     {
         if (*window > *(window - 1)) ++counter;
