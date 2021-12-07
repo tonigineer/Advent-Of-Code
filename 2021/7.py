031297eb080110d84b74908e279f81d7a05dff82
Toni Lubiniecki <toni@lubiniecki.de>
2021-12-07 17:16:44 +0100

    small adjustments to day 7

--- 2021/7.py
+++ 2021/7.py
@@ -1,28 +1,30 @@
 """https://adventofcode.com/2021/day/7."""
 
 
+def intrange(n):
+    """Get sum of integer range."""
+    return int(n*(n+1)/2)
+
+
 def part1(data):
     """Solution for first puzzle.
 
-    Simply (brute force) loop over all position and check against
-    complete range from min and max of positions.
+    Sum distance of all positions to median.
     """
-    fuel_spent = list()
-    positions = list(map(int, data.split(',')))
-    for target in range(min(positions), max(positions)+1):
-        fuel_spent.append(sum([abs(target-position) for position in positions]))
-    return min(fuel_spent)
+    positions = sorted(list(map(int, data.split(','))))
+    median = positions[(len(positions)) // 2]
+    return sum([abs(median - position) for position in positions])
 
 
 def part2(data):
     """Solution to second puzzle.
 
-    Same as part1, except for using sum from 0 to diff instead of diff.
+    Brute force solution with checking all combinations.
     """
     fuel_spent = list()
     positions = list(map(int, data.split(',')))
     for target in range(min(positions), max(positions)+1):
-        fuel_spent.append(sum([sum(range(0, abs(target-position)+1)) for position in positions]))
+        fuel_spent.append(sum([intrange(abs(target-position)) for position in positions]))
     return min(fuel_spent)
 
 
