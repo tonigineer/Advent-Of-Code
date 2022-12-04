d309e7251297aab1cf50d829e34b8df7193cf11d
Toni Lubiniecki (Z590E) <toni@lubiniecki.de>
2022-12-04 11:26:42 +0100

    tile added; cpp added

--- 2022/04/04.py
+++ 2022/04/04.py
@@ -1,7 +1,7 @@
 # https://adventofcode.com/2022/day/4
 
 
-lines = [line.strip() for line in open('2022/04/04.in')]
+lines = [line.strip() for line in open('./2022/04/04.in')]
 
 ans = 0
 ans2 = 0
@@ -17,5 +17,5 @@ for line in lines:
     if s1 <= s2 <= e1 or s1 <= e2 <= e1 or s2 <= s1 <= e2 or s2 <= e1 <= e2:
         ans2 += 1
 
-print(f'1. Solution: {ans}')
-print(f'2. Solution: {ans2}')
+print(f'🎄 1. Solution: {ans}')
+print(f'🎅 2. Solution: {ans2}')
--- README.md
+++ README.md
@@ -10,7 +10,7 @@
 
 <!-- AOC TILES BEGIN -->
 <h1 align="center">
-  2022 - 6 ⭐
+  2022 - 8 ⭐
 </h1>
 <a href="2022/01/01.py">
   <img src="assets/2022/01.png" width="161px">
@@ -20,6 +20,9 @@
 </a>
 <a href="2022/03/03.py">
   <img src="assets/2022/03.png" width="161px">
+</a>
+<a href="2022/04/04.cpp">
+  <img src="assets/2022/04.png" width="161px">
 </a>
 <h1 align="center">
   2021 - 32 ⭐
