819b9896ea4ddfe6618654f0abf49cef9573c189
Z790E_WIN11 <toni@lubiniecki.de>
2023-12-28 17:51:01 +0100

    add: solutions 20/21

--- aoc_2023/src/day_21.rs
+++ aoc_2023/src/day_21.rs
@@ -1,51 +1,152 @@
 use common::{ Answer, Solution };
+use std::collections::{ VecDeque, HashSet };
 
 pub struct Day21;
 
 impl Solution for Day21 {
     fn name(&self) -> &'static str {
-        "Lavaduct Lagoon"
+        "Step Counter"
     }
 
     fn part1(&self, input: &str) -> Answer {
-        return solve(input, false).into();
+        return solve(input, 64).into();
     }
 
     fn part2(&self, input: &str) -> Answer {
-        return solve(input, true).into();
+        return solve2(input, 26501365).into();
     }
 }
 
-fn solve(_input: &str, _part2: bool) -> u64 {
-    return 0
+fn solve2(input: &str, steps: u64) -> u64 {
+    let g: common::Grid = input.into();
+    let mut sr: isize = 0;
+    let mut sc: isize = 0;
+
+    for r in 0..g.grid.len() {
+        for c in 0..g.grid[0].len() {
+            if g.grid[r][c] == 'S' {
+                sr = r as isize;
+                sc = c as isize;
+            }
+        }
+    }
+
+    // Assumptions
+    let size = g.grid.len();
+    assert_eq!(g.grid.len(), g.grid[0].len());
+    assert_eq!(sr, sc);
+    assert_eq!(sr as usize, g.grid.len() / 2);
+
+    let grid_width = steps / size as u64 - 1;
+    // println!("{} {}", size, grid_width);
+
+    let num_odd_grids = (grid_width / 2 * 2 + 1).pow(2);
+    let num_even_grids = ((grid_width + 1) / 2 * 2).pow(2);
+    // println!("{} {}", num_odd_grids, num_even_grids);
+
+    let odd_points = fill(&g, sr, sc, (size * 2 + 1) as u64);
+    let even_points = fill(&g, sr, sc, (size * 2) as u64);
+    // println!("{} {}", odd_points, even_points);
+
+    let corner_top    = fill(&g, size as isize - 1, sc, size as u64 - 1);
+    let corner_right    = fill(&g, sr, 0, size as u64 - 1);
+    let corner_bottom    = fill(&g, 0, sc, size as u64 - 1);
+    let corner_left    = fill(&g, sr, size as isize - 1, size as u64 - 1);
+    // println!("{} {} {} {}", corner_top, corner_right, corner_bottom, corner_left);
+
+    let small_top_right = fill(&g, (size - 1) as isize, 0, (size / 2 - 1) as u64);
+    let small_top_left  = fill(&g, (size - 1) as isize, (size - 1) as isize, (size / 2 - 1) as u64);
+    let small_bottom_right = fill(&g, 0, 0, (size / 2 - 1) as u64);
+    let small_bottom_left = fill(&g, 0, (size - 1) as isize, (size / 2 - 1) as u64);
+    // println!("{} {} {} {}", small_top_right, small_top_left, small_bottom_right, small_bottom_left);
+
+    let large_top_right = fill(&g, (size - 1) as isize, 0, (size * 3 / 2 - 1) as u64);
+    let large_top_left  = fill(&g, (size - 1) as isize, (size - 1) as isize, (size * 3 / 2 - 1) as u64);
+    let large_bottom_right = fill(&g, 0, 0, (size * 3 / 2 - 1) as u64);
+    let large_bottom_left = fill(&g, 0, (size - 1) as isize, (size * 3 / 2 - 1) as u64);
+    // println!("{} {} {} {}", large_top_right, large_top_left, large_bottom_right, large_bottom_left);
+
+    return (
+        num_odd_grids * odd_points as u64 +
+        num_even_grids * even_points as u64 + 
+        (corner_top + corner_right + corner_bottom + corner_left) as u64 +
+        (grid_width + 1) * (small_top_right + small_top_left + small_bottom_right + small_bottom_left) as u64 + 
+        grid_width * (large_top_right + large_top_left + large_bottom_right + large_bottom_left) as u64
+    ) as u64;
 }
 
-// #[cfg(test)]
-// mod tests {
-//     use indoc::indoc;
-//     use super::solve;
+fn solve(input: &str, steps: u64) -> u64 {
+    let g: common::Grid = input.into();
+    let mut sr: isize = 0;
+    let mut sc: isize = 0;
 
-//     const SAMPLE: &str = indoc! {"2413432311323
-// 3215453535623
-// 3255245654254
-// 3446585845452
-// 4546657867536
-// 1438598798454
-// 4457876987766
-// 3637877979653
-// 4654967986887
-// 4564679986453
-// 1224686865563
-// 2546548887735
-// 4322674655533"};
+    for r in 0..g.grid.len() {
+        for c in 0..g.grid[0].len() {
+            if g.grid[r][c] == 'S' {
+                sr = r as isize;
+                sc = c as isize;
+            }
+        }
+    }
 
-//     #[test]
-//     fn example_part1() {
-//         assert_eq!(solve(SAMPLE, false), 102);
-//     }
+    return fill(&g, sr, sc, steps);
+}
 
-//     #[test]
-//     fn example_part2() {
-//         assert_eq!(solve(SAMPLE, true), 94);
-//     }
-// }
+fn fill(g: &common::Grid, sr: isize, sc: isize, steps: u64) -> u64 {
+    let mut ans = HashSet::<(isize, isize)>::new();
+    let mut seen = HashSet::<(isize, isize)>::new();
+    seen.insert((sr, sc));
+
+    let mut q = VecDeque::<(isize, isize, u64)>::new();  // r, c, step remaining
+    q.push_back((sr, sc, steps));
+
+    while !q.is_empty() {
+        let (r, c, s) = q.pop_front().unwrap();
+
+        if s % 2 == 0 { ans.insert((r, c)); }
+        if s == 0 { continue }
+
+        for (dr, dc) in [(1isize, 0isize), (-1, 0), (0, 1), (0, -1)] {
+            let nr = r + dr;
+            let nc = c + dc;
+
+            if nr < 0 || nr >= g.grid.len() as isize { continue }
+            if nc < 0 || nc >= g.grid[0].len() as isize { continue }
+
+            if g.grid[nr as usize][nc as usize] == '#' { continue }
+            if seen.contains(&(nr, nc)) { continue }
+
+            seen.insert((nr, nc));
+            q.push_back((nr, nc, s - 1));
+        }
+    }
+    return ans.len() as u64;
+}
+
+#[cfg(test)]
+mod tests {
+    use indoc::indoc;
+    use super::solve;
+
+    const SAMPLE: &str = indoc! {"...........
+    .....###.#.
+    .###.##..#.
+    ..#.#...#..
+    ....#.#....
+    .##..S####.
+    .##..#...#.
+    .......##..
+    .##.#.####.
+    .##..##.##.
+    ..........."};
+
+    #[test]
+    fn example_part1() {
+        assert_eq!(solve(SAMPLE, 6), 16);
+    }
+
+    // #[test]
+    // fn example_part2() {
+    //     assert_eq!(solve(SAMPLE, true), 94);
+    // }
+}
