76b7eff50542e4c2da79eac7588bf2bb78926370
Z790E_WIN11 <toni@lubiniecki.de>
2023-12-29 17:58:59 +0100

    add: last solutions 23

--- aoc_2023/src/day_22.rs
+++ aoc_2023/src/day_22.rs
@@ -1,10 +1,11 @@
 use common::{ Answer, Solution };
+use std::collections::{ HashMap, HashSet, VecDeque };
 
 pub struct Day22;
 
 impl Solution for Day22 {
     fn name(&self) -> &'static str {
-        "Lavaduct Lagoon"
+        "Sand Slabs"
     }
 
     fn part1(&self, input: &str) -> Answer {
@@ -16,36 +17,135 @@ impl Solution for Day22 {
     }
 }
 
-fn solve(_input: &str, _part2: bool) -> u64 {
-    return 0
+fn overlap(a: &[i64; 6], b: &[i64; 6]) -> bool {
+    return a[0].max(b[0]) <= a[3].min(b[3]) && a[1].max(b[1]) <= a[4].min(b[4]);
 }
 
-// #[cfg(test)]
-// mod tests {
-//     use indoc::indoc;
-//     use super::solve;
+fn falling(mut bricks: Vec<[i64; 6]>) -> Vec<[i64; 6]> {
+    let mut bricks_processed = Vec::<[i64; 6]>::new();
+    for (idx, brick) in bricks.iter_mut().enumerate() {
+        let mut z_max = 1;
+        for next_brick in bricks_processed.iter().take(idx) {
+            if overlap(brick, next_brick) {
+                z_max = z_max.max(next_brick[5] + 1);
+            }
+        }
+        brick[5] -= brick[2] - z_max;
+        brick[2] = z_max;
+        bricks_processed.push(brick.clone())
+    }
+    bricks.sort_by_key(|b| b[2]);
+    return bricks;
 
-//     const SAMPLE: &str = indoc! {"2413432311323
-// 3215453535623
-// 3255245654254
-// 3446585845452
-// 4546657867536
-// 1438598798454
-// 4457876987766
-// 3637877979653
-// 4654967986887
-// 4564679986453
-// 1224686865563
-// 2546548887735
-// 4322674655533"};
+}
 
-//     #[test]
-//     fn example_part1() {
-//         assert_eq!(solve(SAMPLE, false), 102);
-//     }
+fn solve(input: &str, part2: bool) -> u64 {
+    let mut bricks = Vec::<[i64; 6]>::new();
+    for line in input.lines() {
+        let brick: [i64; 6] = line
+            .replace('~', ",")
+            .split(',')
+            .map(|x| x.parse::<i64>().unwrap())
+            .collect::<Vec<i64>>().as_slice().try_into().unwrap();
+        bricks.push(brick);
+    }
 
-//     #[test]
-//     fn example_part2() {
-//         assert_eq!(solve(SAMPLE, true), 94);
-//     }
-// }
+    bricks.sort_by_key(|b| b[2]);
+    bricks = falling(bricks);
+
+    let mut l_supports_u = HashMap::<usize,HashSet<usize>>::new();
+    let mut u_supports_l = HashMap::<usize,HashSet<usize>>::new();
+    for (u, upper) in bricks.iter().enumerate() {
+        for (l, lower) in bricks.iter().take(u).enumerate() {
+            if overlap(lower, upper) && lower[5] + 1 == upper[2] {
+                l_supports_u
+                    .entry(l)
+                    .and_modify(|m| { m.insert(u); })
+                    .or_insert( HashSet::from_iter([u]) );
+                u_supports_l
+                    .entry(u)
+                    .and_modify(|m| { m.insert(l); })
+                    .or_insert( HashSet::from_iter([l]) );
+            }
+        }
+    }
+
+    let mut ans = 0;
+    if !part2 {
+
+        for idx in 0..bricks.len() {
+            if !l_supports_u.contains_key(&idx) { ans += 1; continue }
+        
+            let mut all = true;
+            for u in l_supports_u[&idx].iter() {
+                if u_supports_l[u].len() < 2 {
+                    all = false;
+                }
+            }
+
+            if all { ans += 1 }
+        }
+        return ans;
+    }
+
+    for idx in 0..bricks.len() {
+        let mut q = VecDeque::<usize>::new();
+        let mut falling = HashSet::<usize>::new();
+        falling.insert(idx);
+
+        if !l_supports_u.contains_key(&idx) { continue }
+
+        for u in l_supports_u[&idx].iter() {
+            if !u_supports_l.contains_key(&u) { continue }
+
+            if u_supports_l[&u].len() == 1 {
+                q.push_back(*u);
+                falling.insert(*u);
+            }
+        }
+
+        while !q.is_empty() {
+            let l = q.pop_front().unwrap();
+            if !l_supports_u.contains_key(&l) { continue }
+
+            for l in l_supports_u[&l].iter() {
+                if falling.contains(&l) { continue }
+
+                let mut subset = true;
+                for u in u_supports_l[&l].iter() {
+                    if !falling.contains(u) { subset = false }
+                }
+                if subset {
+                    q.push_back(*l);
+                    falling.insert(*l);
+                }
+            }
+        }
+        ans += (falling.len() - 1) as u64;
+    }
+    return ans as u64;
+}
+
+#[cfg(test)]
+mod tests {
+    use indoc::indoc;
+    use super::solve;
+
+    const SAMPLE: &str = indoc! {"1,0,1~1,2,1
+    0,0,2~2,0,2
+    0,2,3~2,2,3
+    0,0,4~0,2,4
+    2,0,5~2,2,5
+    0,1,6~2,1,6
+    1,1,8~1,1,9"};
+
+    #[test]
+    fn example_part1() {
+        assert_eq!(solve(SAMPLE, false), 5);
+    }
+
+    #[test]
+    fn example_part2() {
+        assert_eq!(solve(SAMPLE, true), 7);
+    }
+}
--- aoc_2023/src/day_23.rs
+++ aoc_2023/src/day_23.rs
@@ -1,10 +1,11 @@
 use common::{ Answer, Solution };
+use std::collections::{ HashSet, HashMap, VecDeque };
 
 pub struct Day23;
 
 impl Solution for Day23 {
     fn name(&self) -> &'static str {
-        "Lavaduct Lagoon"
+        "A Long Walk"
     }
 
     fn part1(&self, input: &str) -> Answer {
@@ -16,8 +17,111 @@ impl Solution for Day23 {
     }
 }
 
-fn solve(_input: &str, _part2: bool) -> u64 {
-    return 0
+fn solve(input: &str, part2: bool) -> u64 {
+    let g: common::Grid = input.into();
+
+    let sr = 0;
+    let sc = g.grid[sr].iter().position(|&r| r == '.').unwrap();
+
+    let er = g.grid.len() - 1;
+    let ec = g.grid[er].iter().position(|&r| r == '.').unwrap();
+
+    let mut junctions = HashSet::<(usize, usize)>::new();
+    junctions.insert((sr, sc));
+    junctions.insert((er, ec));
+
+    for (r, row) in g.grid.iter().enumerate() {
+        for (c, ch) in row.iter().enumerate() {
+            if ch == &'#' { continue }
+            let mut neighbors = 0;
+            for (nr, nc) in [(r+1, c), (r-1, c), (r, c+1), (r, c-1)] {
+                if nr >= g.grid.len() || nc >= g.grid[0].len() { continue }
+                if g.grid[nr][nc] == '#' { continue }
+                neighbors += 1;
+            }
+            if neighbors >= 3 { junctions.insert((r, c)); }
+        }   
+    }
+
+    let mut graph = HashMap::<(usize, usize), HashMap<(usize, usize), usize>>::new();
+    for p in junctions.iter() {
+        graph.insert((p.0, p.1), HashMap::new());
+    }
+
+    for (sr, sc) in junctions.iter() {
+        let mut stack = VecDeque::from([(*sr, *sc, 0)]); 
+        let mut seen = HashSet::<(usize, usize)>::new();
+        seen.insert((*sr, *sc));
+
+
+        while !stack.is_empty() {
+            let (r, c, dist) = stack.pop_front().unwrap();
+
+            if dist != 0 && junctions.contains(&(r, c)) {
+                graph
+                    .entry((*sr, *sc))
+                    .and_modify(|hm| {
+                        hm.insert((r, c), dist);
+                    }
+                    );
+                continue;
+            }
+
+            for (nr, nc) in directions(&g, (r, c), part2) {
+                if nr >= g.grid.len() || nc >= g.grid[0].len() { continue }
+                if g.grid[nr][nc] == '#' { continue }
+                if seen.contains(&(nr, nc)) { continue }
+
+                stack.push_back((nr, nc, dist + 1));
+                seen.insert((nr, nc));
+            }
+        }
+    }
+
+    return dfs(
+        &graph,
+        &mut HashSet::<(usize, usize)>::new(),
+        &(er, ec),
+        &(sr, sc)
+    );
+}
+
+fn dfs(
+    graph: &HashMap<(usize, usize), HashMap<(usize, usize), usize>>,
+    seen: &mut HashSet<(usize, usize)>,
+    end: &(usize, usize), pos: &(usize,usize)) -> u64 
+{
+    if pos == end { return 0 }
+
+    let mut m = u64::MIN;
+    seen.insert(*pos);
+
+    for (next, dist) in graph[pos].iter() {
+        if seen.contains(next) { continue }
+        m = m.max(dfs(&graph, seen, end, &next) + *dist as u64)
+    }
+    seen.remove(pos);
+
+    return m;
+}
+
+fn directions(g: &common::Grid, position: (usize, usize), part2: bool) -> Vec<(usize, usize)> {
+    let r = position.0;
+    let c = position.1;
+
+    if part2 {
+        return [(r+1, c), (r-1, c), (r, c+1), (r, c-1)].to_vec();
+    }
+
+    return match g.grid[position.0][position.1] {
+        '.' => [(r+1, c), (r-1, c), (r, c+1), (r, c-1)].to_vec(),
+        '^' => [(r-1, c)].to_vec(),
+        'v' => [(r+1, c)].to_vec(),
+        '<' => [(r, c-1)].to_vec(),
+        '>' => [(r, c+1)].to_vec(),
+        '#' => [].to_vec(),
+        _ => panic!("Not supposed to happen, ffs")
+    };
 }
 
 // #[cfg(test)]
@@ -25,27 +129,38 @@ fn solve(_input: &str, _part2: bool) -> u64 {
 //     use indoc::indoc;
 //     use super::solve;
 
-//     const SAMPLE: &str = indoc! {"2413432311323
-// 3215453535623
-// 3255245654254
-// 3446585845452
-// 4546657867536
-// 1438598798454
-// 4457876987766
-// 3637877979653
-// 4654967986887
-// 4564679986453
-// 1224686865563
-// 2546548887735
-// 4322674655533"};
+//     const SAMPLE: &str = indoc! {
+// "#.#####################
+// #.......#########...###
+// #######.#########.#.###
+// ###.....#.>.>.###.#.###
+// ###v#####.#v#.###.#.###
+// ###.>...#.#.#.....#...#
+// ###v###.#.#.#########.#
+// ###...#.#.#.......#...#
+// #####.#.#.#######.#.###
+// #.....#.#.#.......#...#
+// #.#####.#.#.#########v#
+// #.#...#...#...###...>.#
+// #.#.#v#######v###.###v#
+// #...#.>.#...>.>.#.###.#
+// #####v#.#.###v#.#.###.#
+// #.....#...#...#.#.#...#
+// #.#########.###.#.#.###
+// #...###...#...#...#.###
+// ###.###.#.###v#####v###
+// #...#...#.#.>.>.#.>.###
+// #.###.###.#.###.#.#v###
+// #.....###...###...#...#
+// #####################.#"};
 
 //     #[test]
 //     fn example_part1() {
-//         assert_eq!(solve(SAMPLE, false), 102);
+//         assert_eq!(solve(SAMPLE, false), 94);
 //     }
 
 //     #[test]
 //     fn example_part2() {
-//         assert_eq!(solve(SAMPLE, true), 94);
+//         assert_eq!(solve(SAMPLE, true), 154);
 //     }
 // }
